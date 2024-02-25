88bd743b893b730e6c76b7f590aeac436743d424
Lasu-1997 <lasudaraka97@gmail.com>
2024-02-25 22:31:43 +0530

    Added student model class

---
+++ Student-Management-System-API/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student_Management_System_API.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string FirstName { get; set; } = "";
+
+        [MaxLength(100)]
+        public string LastName { get; set; } = "";
+        public DateTime DateOfBirth { get; set; }
+
+        [MaxLength(100)]
+        public string Address { get; set; } = "";
+
+    }
+}
