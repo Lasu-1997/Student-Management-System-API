39e1b97c3142e0ba6e355ef8d36c4c2387967356
Shehan Udaraka <shehantharuprocessing@gmail.com>
2024-02-26 11:23:29 +0530

    Added dbcontext class

---
+++ Student-Management-System-API/Services/StudentDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Management_System_API.Models;
+
+namespace Student_Management_System_API.Services
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
